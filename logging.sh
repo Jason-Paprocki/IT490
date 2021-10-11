cbba0b5fa72b1a569d63aabd8fc18d8201442714
Jason-Paprocki <jasonpaprocki@tutanota.com>
2021-10-11 14:15:26 -0400

    update logging script

--- logging.sh
+++ logging.sh
@@ -4,9 +4,9 @@ sudo cp /var/log/syslog /tmp/syslog-NAME
 sudo cp -r /var/log/apache2 /tmp/apache2-NAME
 sudo cp -r /var/log/rabbitmq /tmp/rabbitmq-NAME
 
-scp /tmp/syslog-NAME ubuntu@172.28.79.194:~/log/syslog
-scp /tmp/apache2-NAME ubuntu@172.28.79.194:~/log/apache2
-scp /tmp/rabbitmq-NAME ubuntu@172.28.79.194:~/log/rabbitmq
+scp /tmp/syslog-NAME ubuntu@172.28.79.194:~/log/syslog-NAME
+scp /tmp/apache2-NAME ubuntu@172.28.79.194:~/log/apache2-NAME
+scp /tmp/rabbitmq-NAME ubuntu@172.28.79.194:~/log/rabbitmq-NAME
 
 
 rm -rf /tmp/*
