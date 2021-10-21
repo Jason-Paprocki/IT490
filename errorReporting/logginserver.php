c1e65e44c63a512888fbe72bb8ecf4145398a3c0
Jason.Paprocki <jason.paprocki@tutanota.com>
2021-10-20 20:08:45 -0400

    changed to update this

---
+++ errorReporting/logginserver.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function doLogin($username,$password)
+{
+    // lookup username in databas
+    // check password
+    return true;
+    //return false if not valid
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  $fp = fopen('error.log', 'a');
+  $current_timestamp = date('Y-m-d');
+  $log = strval($current_timestamp)." ".$request["message"]."\n";
+  fwrite($fp,$log);
+  fclose($fp);  
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
