4a9897aa147010a0f6fb57bc31757c94874bca21
Joshua Jen <72992886+jjen455@users.noreply.github.com>
2021-10-26 17:36:50 -0400

    Add files via upload
    new working php example

--- backend/RescueGroups.php
+++ backend/RescueGroups.php
@@ -1,125 +1,103 @@
 <?php
-// CHANGE THIS TO MATCH YOUR ENVIRONMENT
-const DB_HOST = "localhost";
-const DB_DATABASE = "dogedb"; // the name of your database
-const DB_USER = "joshua";        // the user or your database
-const DB_PASSWORD = "password";   // the password of your database
+$httpApiUrl = "https://api.rescuegroups.org/http/v2.json";
+$data = array(
+    "apikey" => "Li91u8iK",
+    "objectType" => "animals",
+    "objectAction" => "publicSearch",
+    "search" => array (
+        "calcFoundRows" => "Yes",
+        "resultStart" => 0,
+        "resultLimit" => 20,
+        "resultSort" => "animalID",
+        "fields" => array("animalID","animalOrgID","animalName","animalSpecies","animalBreed","animalThumbnailUrl"),
+        "filters" => array (
+            array(
+                "fieldName" => "animalStatus",
+                "operation" => "equals",
+                "criteria" => "Available"
+            )
+        )
+    )
+);
 
-// CREDENTIALS
-const API_CLIENT_ID = 'XXXXXXXX';
-// const API_CLIENT_SECRET = 'fb301c94b4a44f8286a26a268163f8c2';
-
-// Variables
-//$redirect_uri = 'http://localhost:8888/callback'; // not sure what the redirect url should be?
-
-// Helper functions
-function encode($client_id, $client_secret) {
-    return base64_encode("$client_id:$client_secret");
+$result = postToApi($data);
+if (!$result) {
+    echo "login issue with the API.";
+    exit;
 }
 
-function MakeAuthorizationHeaders($authorization) {
-    return [
-        "Authorization: Basic {$authorization}",
-        "Content-Type: application/json"
-    ];
-}
-
-function CallApi($token_url, $token_headers, $token_content) {
+print_r($result);
+exit;
+ 
+function postJson($url, $json) {
+    // create a new cURL resource
     $ch = curl_init();
-    
-    // set options, url, etc
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $token_headers);
-    curl_setopt($ch, CURLOPT_URL, $token_url);
-    
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $token_content);
+    // set options, url, etc.
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
     curl_setopt($ch, CURLOPT_POST, 1);
-    
     //curl_setopt($ch, CURLOPT_VERBOSE, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
+    // grab URL and pass it to the browser
     $result = curl_exec($ch);
-    
     if (curl_errno($ch)) {
-        $results = curl_error($ch);
+        // TODO: Handle errors here
+        return array(
+                "result" => "",
+                "status" => "error",
+                "error" => curl_error($ch)
+        );
     } else {
+        // close cURL resource, and free up system resources
         curl_close($ch);
-        $results = $result;
     }
-
-    $resultsArray = json_decode($results);
-
-    print_r($resultsArray);
+    return array(
+            "status" => "ok",
+            "error" => "",
+            "result" => $result,
+    );
+}
+function postToApi($data) {
+    $resultJson = postJson($GLOBALS["httpApiUrl"], json_encode($data));
+    if ($resultJson["status"] == "ok") {
+        $result = json_decode($resultJson["result"], true);
+        $jsonError = getJsonError();
+        if (!$jsonError && $resultJson["status"] == "ok") {
+            return $result;
+        } else {
+            return array (
+                    "status" => "error",
+                    "text" => $result["error"] . $jsonError,
+                    "errors" => array()
+            );
+        }
+    } else return false;
 }
 
-function SaveToDatabase($username, $names, $ages, $breeds, $colors) {
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    foreach ($names as $name) {
-        $animalname = $name['name'];
-        $sql = "INSERT INTO animaldatabase(username, animalname) VALUES ('${username}', '${animalname}')";
-        if (! mysqli_query($conn, $sql)) {
-            die("Error: " . $sql . "" . mysqli_error($conn));
-        }
-    }
-
-    foreach ($ages as $age) {
-        $animalage = $age['age'];
-        $sql = "INSERT INTO animaldatabase(username, animalage) VALUES ('${username}', '${animalage}')";
-        if (! mysqli_query($conn, $sql)) {
-            die("Error: " . $sql . "" . mysqli_error($conn));
-        }
-    }
-
-    foreach ($breeds as $breed) {
-        $animalbreed = $breed['breed'];
-        $sql = "INSERT INTO animaldatabase(username, animalbreed) VALUES ('${username}', '${animalbreed}')";
-        if (! mysqli_query($conn, $sql)) {
-            die("Error: " . $sql . "" . mysqli_error($conn));
-        }
-    }
-
-    foreach ($colors as $color) {
-        $animalcolor = $color['color'];
-        $sql = "INSERT INTO animaldatabase(username, animalcolor) VALUES ('${username}', '${animalcolor}')";
-        if (! mysqli_query($conn, $sql)) {
-            die("Error: " . $sql . "" . mysqli_error($conn));
-        }
-    }
-    mysqli_close($conn);
-}
-
-function GetAnimalColor(){
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    {
-        "apikey": "Li91u8iK",
-        "objectType":"animalColors",
-        "objectAction":"publicSearch",
-        "search":
-        {
-            "resultStart": "0",
-            "resultLimit": "100",
-            "resultSort": "colorName",
-            "resultOrder": "asc",
-            "filters":
-            [
-                {
-                    "fieldName": "colorSpecies",
-                    "operation": "equals",
-                    "criteria": "Dog"
-                }
-            ],
-            "filterProcessing": "1",
-            "fields": ["colorID","colorName","colorSpecies","colorSpeciesID"]
-        }
+function getJsonError() {
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            return false;
+            break;
+        case JSON_ERROR_DEPTH:
+            return "Maximum stack depth exceeded";
+            break;
+        case JSON_ERROR_STATE_MISMATCH:
+            return "Underflow or the modes mismatch";
+            break;
+        case JSON_ERROR_CTRL_CHAR:
+            return "Unexpected control character found";
+            break;
+        case JSON_ERROR_SYNTAX:
+            return "Syntax error, malformed JSON";
+            break;
+        case JSON_ERROR_UTF8:
+            return "Malformed UTF-8 characters, possibly incorrectly encoded";
+            break;
+        default:
+            return "Unknown error";
+            break;
     }
 }
 ?>
