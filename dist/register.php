8ff6303881f6251b39fe303d79784d881794b9be
Jason-Paprocki <jasonpaprocki@tutanota.com>
2021-10-26 16:41:35 -0400

    dog ass chabged

--- dist/register.php
+++ dist/register.php
@@ -19,7 +19,7 @@
 	require_once('get_host_info.inc');
 	require_once('rabbitMQLib.inc');
 
-	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+	$client = new rabbitMQClient("testRabbitMQ.ini","frontbackcomms");
 	$request = array();
 	$request['type'] = "Register";
 	$request['username'] = $uname;
