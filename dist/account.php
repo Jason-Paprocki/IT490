ec32d419aa05e33701882c04c73f0675c3604164
Chinonso Anyanka <cc792@njit.edu>
2021-10-27 12:19:46 -0400

    updated account.php

---
+++ dist/account.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+?>
+
+<html>
+
+
+
+
+<form action="logout.php">
+    <input type="submit" value="Log Out">
+</form>
+
+</html>
