b9696617416fa5ef0631c736237cd503b2cb0343
Joshua Jen <72992886+jjen455@users.noreply.github.com>
2021-10-29 16:07:10 -0400

    Update medical.php

--- dist/medical.php
+++ dist/medical.php
@@ -15,7 +15,33 @@
 //vet address
 
 //will pull this data by checking the id and matching it to the id set in the Users table
+try{
+	$db = new PDO($connection_string, $dbuser, $dbpass);
+	echo "Created to create Users table\n";
+	$stmt = $db->prepare("create table if not exists `Users` (
+				`id` int auto_increment not null,
+				`email` varchar(100) not null unique,
+                `password` varchar(100) not null,
+                `fname` varchar(20) not null,
+                `lname` varchar(20) not null,
+				PRIMARY KEY (`id`)
+				) CHARACTER SET utf8 COLLATE utf8_general_ci"
+			);
+	$stmt->execute();
+	echo var_export($stmt->errorInfo(), true);
+}
+catch(Exception $e){
+    echo $e->getMessage();
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    $request = array();
+    $request['type'] = "Error";
+    $request['message'] = $e;
+    //$response = $client->send_request($request);
+    $response = $client->publish($request);
 
+    echo "sent error".PHP_EOL;
+	exit("It didn't work");
+}
 //will need to be able to edit this data
 
 //picture of the animal?
