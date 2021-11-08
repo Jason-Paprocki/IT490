740e9217c220f7448346cac0ef28a9ebab84a2d7
lakhvir211 <60156310+lakhvir211@users.noreply.github.com>
2021-11-08 07:53:34 -0500

    updated, vaccine scheduled based on vet dob

--- dist/medical.php
+++ dist/medical.php
@@ -1,103 +1,170 @@
 <?php
-//check if cookie is set
-if(!isset($_COOKIE['id'])){
-    header("/login.php");
-    exit();
-}
+// if login
 
-//prob going to need a table for medical info
-try{
-	$db = new PDO($connection_string, $dbuser, $dbpass);
-	echo "Created to create Medical Info table\n";
-	$stmt = $db->prepare("create table if not exists `Medical` (
-				`id` int auto_increment not null,
-				`email` varchar(100) not null unique,
-                `password` varchar(100) not null,
-                `fname` varchar(20) not null,
-                `lname` varchar(20) not null,
-				PRIMARY KEY (`id`)
-				) CHARACTER SET utf8 COLLATE utf8_general_ci"
-			);
-	$stmt->execute();
-	echo var_export($stmt->errorInfo(), true);
-}
-catch(Exception $e){
-    echo $e->getMessage();
-    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-    $request = array();
-    $request['type'] = "Error";
-    $request['message'] = $e;
-    //$response = $client->send_request($request);
-    $response = $client->publish($request);
+// if (!isset($_COOKIE['id'])) {
+//     header("/login.php");
+//     exit();
+// }
 
-    echo "sent error".PHP_EOL;
-	exit("It didn't work");
-}
-
-    //display
-//name of customer
-//name of pet
-//type of pet
-// vaccines
-//gromming
-//vet checkup times
-//vet name
-//vet phone
-//vet address
-
-//will pull this data by checking the id and matching it to the id set in the Users table
-//will need to be able to edit this data
-
-//picture of the animal?
-
-//pull display info from database
-//set all these variables in php and pass them to the html
+// creaating table if not exist
+// if form submited 
 
 
+$vaccine = array(
+    array('name' => "DHLPPC", "dosage" => "First ", "duration" => "6 to 8 week", "days" => "+ 6 weeks"),
+    array('name' => "DHLPPC", "dosage" => "Second ", "duration" => "9 to 11 week", "days" => "+9 weeks"),
+    array('name' => "DHLPPC", "dosage" => "First ", "duration" => "12 to 14 week", "days" => "+ 12 weeks"),
+    array('name' => "DHLPPC", "dosage" => "First ", "duration" => "16 to 17 week", "days" => "+ 16 weeks"),
+    array('name' => "DHLPPC", "dosage" => "Boster ", "duration" => "every 12 Months", "days" => "+1 year"),
+    array('name' => "Bordetella", "dosage" => "First ", "duration" => "14 weeks", "days" => "+ 14 weeks"),
+    array('name' => "Bordetella", "dosage" => "Second ", "duration" => "6 to 12 Months", "days" => "+1 years"),
+    array('name' => "Rabies", "dosage" => "First ", "duration" => "16 weeks", "days" => "+16 weeks"),
+    array('name' => "Rabies", "dosage" => "Booster  ", "duration" => "every 12 to 36 months", "days" => "+2 year"),
+    array('name' => "Rabies", "dosage" => "First ", "duration" => "16 weeks", "days" => "+16 weeks"),
+    array('name' => "Lyme", "dosage" => "First ", "duration" => "14 weeks", "days" => "+14 weeks"),
+    array('name' => "Lyme", "dosage" => "Second  ", "duration" => "17 weeks", "days" => "+17 weeks"),
+    array('name' => "Lyme", "dosage" => "Booster  ", "duration" => "every 12 month", "days" => "+1 years"),
+);
 
 
+$sql = "CREATE TABLE IF NOT EXISTS OrderItems(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    pet_name varchar(50), 
+    pet_type varchar(50), 
+    checkup varchar(50), 
+    checkup_date varchar(50), 
+    category varchar(50), 
+    category varchar(50), 
+    category varchar(50), 
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )";
 
-
-
-
-//html
-//will need to make a table for the medical info
-//picture can be small in the corner top right or left
-//will need to be able to edit this data -- edit button or something?
-//how can we create push notifications? -- send to email or just javascript at login?
-
-//need to create medical.html for vet search page - Joshua
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html>
-<html lang="en" >
+
 <head>
-  <meta charset="UTF-8">
-  <title>Homepage</title>
-  <link rel="icon" href="https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/XA6KIXE6FBFM5EWSA25JI5YAU4.jpg"/>
-  <link rel="stylesheet" href="./style.css">
+    <title>Medical Services</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/XA6KIXE6FBFM5EWSA25JI5YAU4.jpg" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 </head>
+
 <body>
-<!-- partial:index.partial.html -->
-<html>
-  <head>
-    <title>Parallax Template - uplusion23</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-smooth-scroll/1.5.5/jquery.smooth-scroll.min.js"></script>
-  </head>
-  <body>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
     <div class="navbar">
-      <ul class="navbar-container">
-        <li><a href="index.html" class="left-underline nav-button brand-logo">Pet Adoption Service</a></li>
-	<li class="nav-item active"><a href="login.html" class="left-underline nav-button" data-scroll>Account</a></li>
-        <li class="nav-item"><a href="#section-3" class="left-underline nav-button" data-scroll>Wallet</a></li>
-        <li class="nav-item"><a href="#section-2" class="left-underline nav-button" data-scroll>Services</a></li>
-	<li class="nav-item active"><a href="forum.html" class="left-underline nav-button" data-scroll>Forums</a></li>
-	<li class="nav-item active"><a href="medical.html" class="left-underline nav-button" data-scroll>Medical Info</a></li>
-	      </body>
+        <ul class="navbar-container">
+            <li><a href="index.html" class="left-underline nav-button brand-logo">Pet Adoption Service</a></li>
+            <li class="nav-item active"><a href="login.php" class="left-underline nav-button" data-scroll>Account</a></li>
+            <li class="nav-item"><a href="#section-3" class="left-underline nav-button" data-scroll>Wallet</a></li>
+            <li class="nav-item"><a href="#section-2" class="left-underline nav-button" data-scroll>Services</a></li>
+            <li class="nav-item active"><a href="forum.php" class="left-underline nav-button" data-scroll>Forums</a></li>
+        </ul>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <h1>Calculate your dog vaccination schedule </h1>
+            <form action="">
+                <div class="form-group">
+                    <label for="dob">Dog Date of birth:</label>
+                    <input type="date" class="form-control" name="dob" placeholder="dob" id="dob">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Get Scheduled</button>
+            </form>
+        </div>
+        <table class="table table-bordered table-striped table-dark">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Vaccine Name</th>
+                    <th>Dosage</th>
+                    <th>Duration</th>
+                    <?php if (isset($_GET['dob'])) { ?> <th>Your Date</th>
+                        <th>Action</th> <?php  } ?>
+                </tr>
+
+            </thead>
+            <tbody>
+                <?php $i = 0;
+
+                foreach ($vaccine as $v) { ?>
+                    <tr>
+                        <td><strong><?php echo ++$i; ?> </strong></td>
+                        <td><?php echo $v['name'] ?></td>
+                        <td><?php echo $v['dosage'] ?></td>
+                        <td><?php echo $v['duration'] ?></td>
+                        <?php if (isset($_GET['dob'])) { ?> <td> <?php echo date('D, Y-M-d', strtotime($_GET['dob'] . " " . $v['days'])); ?> </td>
+
+                            <td> <button class="btn btn-primary">Notify me</button></td>
+                        <?php } ?>
+
+                    </tr>
+
+
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- <div class="parallax p1" id="section-1">
+        <hgroup>
+            <h1>Login</h1>
+            <form name="med_form" id="med_form" method="POST">
+                <fieldset>
+                    <legend><strong>Pet Information</strong></legend>
+                    <label for="pet_name">Pet Name:</label><br>
+                    <input type="text" id="pet_name" name="pet_name" placeholder="Enter Pet Name"><br><br>
+                    <label for="pet_name">Pet Type:</label><br>
+                    <input list="pet_type" name="pet_type" /></label>
+                    <datalist id="pet_type">
+                        <option value="Cat">
+                        <option value="Dog">
+                        <option value="Birds">
+                        <option value="Rabbit">
+                        <option value="Fish">
+                        <option value="Pet Livestock">
+                        <option value="Pet Poultry">
+                    </datalist><br><br>
+                </fieldset><br>
+                <fieldset>
+                    <label for="checkup">Checkup Type:</label><br>
+                    <legend><strong>Schedule</strong></legend>
+                    <select name="checkup" id="checkup">
+                        <option value="">Select Checkup type</option>
+                        <option value="routine">Routine</option>
+                        <option value="annual">Annual</option>
+                        <option value="emergency">Emergency</option>
+                    </select><br><br>
+
+
+
+                    <label for="checkup_date">Check up Date:</label><br>
+                    <input type="date" id="checkup_date" name="checkup_date" placeholder="Select A Date"><br><br>
+
+                    <label for="checkup_time">Check up Time:</label><br>
+                    <input type="time" id="checkup_time" name="checkup_time" placeholder="Select A Time"><br><br>
+
+
+                </fieldset><br>
+
+                <fieldset>
+                    <legend><strong>Contact Info</strong></legend>
+                    <label for="address">Address:</label><br>
+                    <input type="text" name="address" id="address" placeholder="Enter Address"><br><br>
+                    <label for="contact">Contact Info:</label><br>
+                    <input type="text" name="contact" id="contact" placeholder="Enter Mobile Number"><br>
+                </fieldset><br>
+
+                <input type="submit" name="submit" value="submit">
+            </form>
+        </hgroup>
+    </div> -->
+</body>
+
 </html>
