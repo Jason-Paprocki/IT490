27233ec4e4249b3a68a4040a8504b7e675379a21
Akram Katideen <akramkatideen@akrams-mbp.resource.campus.njit.edu>
2021-12-18 14:09:24 -0500

    Updated for highlight feature

--- backend/sql_files/forum.sql
+++ backend/sql_files/forum.sql
@@ -1,3 +1,6 @@
+-- added the column user_id and make it foreign key with the id of users table
+
+
 create table if not exists `Forum` (
 `fname` varchar(20) not null,
 `lname` varchar(20) not null,
@@ -7,3 +10,12 @@ create table if not exists `Forum` (
 `message` varchar(1000) not null,
 PRIMARY KEY (`post_id`)
 ) CHARACTER SET utf8 COLLATE utf8_general_ci
+
+
+
+ALTER TABLE `forum`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+ALTER TABLE `forum`
+  ADD CONSTRAINT `forum_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
